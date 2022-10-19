0aeb65a4e81ee6b287c530212532f7cb20230ada
Anouar9172 <94382980+Anouar9172@users.noreply.github.com>
2022-10-19 13:14:13 +0200

    1

--- kamadoing/index.php
+++ kamadoing/index.php
@@ -16,7 +16,7 @@
         </div>
       <ul>
         <li><a href="index.php" class="underline">Home</a></li>
-        <li><a href="contact.php.html">contact</a></li>
+        <li><a href="contact.php">contact</a></li>
         <li><a href="info.php">info</a></li>
         <li><a href="blog.php">blog</a></li>
       </ul>
--- kamadoing/info.php
+++ kamadoing/info.php
@@ -1,8 +1,13 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+        <meta http-equiv="language" content="NL">
+        <meta name="viewport" content="width=device-width, intial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Anouar/Zakaria/Omar">
+        <meta name="keywords" content="">
+        <title>kabadoing</title>
     <link rel="stylesheet" href="css/style.css">
-    <title>kabadoing</title>
   </head>
   <body>
     <nav>
@@ -11,7 +16,7 @@
         </div>
       <ul>
         <li><a href="index.php" class="underline">Home</a></li>
-        <li><a href="contact.php.html">contact</a></li>
+        <li><a href="contact.php">contact</a></li>
         <li><a href="info.php">info</a></li>
         <li><a href="blog.php">blog</a></li>
       </ul>
