0e2d903926494a0673a4418b683222715b5e9c86
Anouar9172 <94382980+Anouar9172@users.noreply.github.com>
2022-11-15 22:02:29 +0100

    volledige contact pagina v1

--- kamadoing/contact.php
+++ kamadoing/contact.php
@@ -1,29 +1,30 @@
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-        <meta http-equiv="language" content="NL">
-        <meta name="viewport" content="width=device-width">
-        <meta name="description" content="">
-        <meta name="author" content="Anouar/Zakaria/Omar">
-        <meta name="keywords" content="">
-        <title>kabadoing</title>
-    <link rel="stylesheet" href="css\stylesheet.css">
-  </head>
-  <body>
-    <nav>
-      <div class="logo">
-        <img src="img\goedelogo.png" alt="">
-        </div>
-      <ul>
-        <li><a href="index.php" class="underline">Home</a></li>
-        <li><a href="contact.php">contact</a></li>
-        <li><a href="info.php">info</a></li>
-        <li><a href="blog.php">blog</a></li>
-      </ul>
-
-    </nav>
-<main>
-
-</main>
-  </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Contact Me</h1>
+        <p>Feel free to contact us and we will get back to you ass sson as we can.</p>
+        <form action="mail.php" method="POST">
+            <label for="name">Name:</label>
+            <input type="text" name="name" id="name">
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email">
+            <label for="subject">Subject:</label>
+            <input type="text" name="subject" id="subject">
+            <label for="message">Message</label>
+            <textarea name="message" cols="30" rows="10"></textarea>
+            <input type="submit" value="Send">
+        </form>
+    </div>
+</body>
 </html>
+
+
