ff32d473a3cd1f3d48872dda380a9147fd19849f
Anouar9172 <94382980+Anouar9172@users.noreply.github.com>
2022-11-15 22:10:04 +0100

    update

--- kamadoing/contact.php
+++ kamadoing/contact.php
@@ -4,22 +4,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact form</title>
+    <title>Kamadoing</title>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css\style.css">
 </head>
 <body>
+  <nav>
+    <div class="logo">
+      <img src="img\goedelogo.png" alt="">
+      </div>
+    <ul>
+      <li><a href="index.php" class="underline">Home</a></li>
+      <li><a href="contact.php">Contact</a></li>
+      <li><a href="info.php">Info</a></li>
+      <li><a href="blog.php">Blog</a></li>
+    </ul>
+
+  </nav>
     <div class="container">
         <h1>Contact Me</h1>
-        <p>Feel free to contact us and we will get back to you ass sson as we can.</p>
+        <p>Wees altijd open me te berichten en ik zal zo snel mogelijk met u in contact kopen</p>
         <form action="mail.php" method="POST">
-            <label for="name">Name:</label>
+            <label for="name">Naam:</label>
             <input type="text" name="name" id="name">
             <label for="email">Email:</label>
             <input type="email" name="email" id="email">
-            <label for="subject">Subject:</label>
+            <label for="subject">Onderwer[]:</label>
             <input type="text" name="subject" id="subject">
-            <label for="message">Message</label>
+            <label for="message">Bericht</label>
             <textarea name="message" cols="30" rows="10"></textarea>
             <input type="submit" value="Send">
         </form>
